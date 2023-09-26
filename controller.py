20ef2b63a2f24ddf30a0f31d91fef667380a4916
Officialahmed <Ahmed.riyami1818@gmail.com>
2023-09-26 21:10:22 +0400

    UPDATED: SEARCH ALGORITHM

--- controller.py
+++ controller.py
@@ -22,8 +22,8 @@ class Cache:
 
     def __init__(self) -> None:
 
-        self.file_finder = Finder.File
-        self.folder_finder = Finder.Folder
+        self.file_finder = Finder.File()
+        self.folder_finder = Finder.Folder()
         self.trash_folder_path = "trash\\"
 
         self.trash_content_file = f"{self.trash_folder_path}content.json"
@@ -37,22 +37,22 @@ class Cache:
         self.folder_finder.set_recursive(is_recursive)
 
 
-    def get_files_by_name(self, name:str) -> list:
-        print(self.file_finder.by_name(name))
+    def get_files_by_name(self, name:str) -> dict:
+        print(self.file_finder.find("NAME", name))
 
 
-    def get_files_by_extension(self, extension:str) -> list:
-        print(self.file_finder.by_extention(extension))
+    def get_files_by_extension(self, extension:str) -> dict:
+        print(self.file_finder.find("EXTENSION", extension))
 
 
-    def get_files_by_pattern(self, pattern:str) -> list:
-        print(self.file_finder.by_pattern(pattern))
+    def get_files_by_pattern(self, pattern:str) -> dict:
+        print(self.file_finder.find("PATTERN", pattern))
 
 
-    def get_folders_by_name(self, name:str) -> list:
-        print(self.folder_finder.by_name(name))
+    def get_folders_by_name(self, name:str) -> dict:
+        print(self.folder_finder.find("NAME", name))
 
 
-    def get_folders_by_pattern(self, pattern:str) -> list:
-        print(self.folder_finder.pattern(pattern))
+    def get_folders_by_pattern(self, pattern:str) -> dict:
+        print(self.folder_finder.find("PATTERN", pattern))
 
