211b4f5d5f7130d7fbadb52b536e8390a6ee5ca4
OfficialAhmed <52674147+OfficialAhmed@users.noreply.github.com>
2023-12-13 02:06:06 +0400

    FIXED: UI BUGS
    * ADDED: RENAME FEATURE UI
    
    * FIXED: RENAME FEATURE TABLE
    
    * UPDATED: UI
    
    * FIXED: UI
    
    * ENHANCED: UI
    
    * ENHANCED: CODE FACTORIZATION

--- Interface/environment.py
+++ Interface/environment.py
@@ -33,12 +33,208 @@ from PySide6.QtWidgets import *
 
 from datetime import datetime
 from controller import Controller
-
+import os, json
 
 class Common:
+    """
+        COMMON METHODS BETWEEN FEATURES
+    """
 
     def __init__(self) -> None:
         self.constant = Constant()
+        
+        self.html = Html()
+        self.constant = Constant()
+        self.controller = Controller()
+        self.progressBar = ProgressBar()
+        self.controller.update_remover_param()
+
+        self.path_input = ""
+        self.cache_file = self.controller.CACHE_FILE
+
+        # CREATE DATA FOLDER IF NOT FOUND
+        if not os.path.exists("data"):
+            os.mkdir(self.controller.DATA_PATH)
+
+    def set_controller_widgets(
+        self,
+        lookupType:            QPushButton,
+        currentPath:           QLineEdit,
+        lookupFormat:          QLineEdit,
+        lookupInput:           QCheckBox,
+        isRecursive:           QLineEdit,
+        startBtn:              QComboBox
+    ):
+        """
+        Set current window widgets from 'UI' class
+        """
+        self.startBtn:         QPushButton = startBtn
+        self.lookupType:       QComboBox = lookupType
+        self.lookupInput:      QLineEdit = lookupInput
+        self.isRecursive:      QCheckBox = isRecursive
+        self.lookupFormat:     QComboBox = lookupFormat
+        self.currentPathInput: QLineEdit = currentPath
+
+    def import_cache(self) -> None:
+
+        # IF CACHE FOUND
+        if os.path.exists(self.controller.CACHE_FILE):
+            cache: dict = json.load(open(self.cache_file)).get("delete page")
+
+            self.lookupInput.setText(cache.get("lookupInput"))
+            self.isRecursive.setChecked(cache.get("IsRecursive"))
+            self.currentPathInput.setText(cache.get("currentPath"))
+            self.lookupType.setCurrentText(cache.get("lookupType"))
+
+            self.path_input = cache.get("currentPath")
+
+            # UPDATE LOOKUP FORMAT ACCORDING TO THE CACHED LOOKUP TYPE
+            self.change_lookup_format()
+
+            self.lookupFormat.setCurrentText(cache.get("lookupFormat"))
+
+    def export_cache(self) -> None:
+
+        # TODO: CHECK IF BOTH CACHES ARE THE SAME, SKIP EXPORTING - NO NEED TO WASTE TIME IN WRITING
+        cache = {}
+
+        # IF CACHE FOUND
+        if os.path.exists(self.cache_file):
+            cache: dict = json.load(open(self.cache_file))
+
+        search = self.lookupInput.text()
+        path = self.currentPathInput.text()
+        lu_type = self.lookupType.currentText()
+        recursive = self.isRecursive.isChecked()
+        lu_frmt = self.lookupFormat.currentText()
+
+        cache["delete page"] = {
+            "lookupType":   lu_type,
+            "currentPath":  path,
+            "lookupFormat": lu_frmt,
+            "lookupInput":  search,
+            "IsRecursive":  recursive,
+        }
+
+        with open(self.cache_file, "w+") as file:
+            json.dump(cache, file)
+
+    def set_user_path(
+        self, path: str,
+        is_changed_manually: bool
+    ) -> None:
+        """
+        Update user path input
+        """
+
+        # RESET USER PATH
+        if not path:
+            self.path_input = ""
+            return
+
+        self.path_input = path
+
+        if not is_changed_manually:
+            self.currentPathInput.setText(path)
+
+    def change_lookup_format(self) -> None:
+        """
+        Change lookup format options according to the lookup type
+        """
+
+        current_type = self.lookupType.currentText()
+
+        # REMOVE ALL TYPES
+        self.lookupFormat.clear()
+
+        # GENERATE NEW TYPES
+        new_formats = (
+            "NAME",
+            "PATTERN"
+        )
+
+        # ADD THE OPTION 'EXTENSION' IF 'FILES' SELECTED
+        if current_type == "FILES":
+            new_formats = (
+                "NAME",
+                "PATTERN",
+                "EXTENSION"
+            )
+
+        for format in new_formats:
+            self.lookupFormat.addItem(format)
+
+    def get_data(self) -> (dict, bool):
+        """
+        ### Begin lookup process. 
+            * Deactivate all btns and reactivate upon end of process 
+        """
+
+        input: str = self.lookupInput.text()
+        type: str = self.lookupType.currentText()
+        format: str = self.lookupFormat.currentText()
+        is_recursive: bool = self.isRecursive.isChecked()
+
+        # UPDATE THE FINDER PARAMETERS
+        self.controller.update_finder_param(
+            self.path_input,
+            is_recursive,
+        )
+
+        data = {}
+
+        # TERMINATE IF EITHER INPUTS ARE EMPTY
+        if not input or not self.path_input:
+            self.controller.show_dialog(
+                "SEARCH INPUT IS EMPTY!",
+                "w",
+                is_dialog=False
+            )
+
+            return (data, True)                 # FAILED = True
+
+        # TERMINATE IF ENTERED PATH CANNOT BE FOUND
+        if not os.path.exists(self.currentPathInput.text()):
+            self.controller.show_dialog(
+                "FOLDER DOESN'T EXIST. DOUBLE CHECK THE ENTERED PATH",
+                "w",
+                is_dialog=False
+            )
+
+            return (data, True)                 # FAILED = True
+
+        try:
+            # SEARCH BY SELECTED FORMAT
+            if type == "FILES":
+                match format:
+                    case "NAME":
+                        data = self.controller.get_files_by_name(input)
+
+                    case "EXTENSION":
+                        data = self.controller.get_files_by_extension(input)
+
+                    case "PATTERN":
+                        data = self.controller.get_files_by_pattern(input)
+
+            elif type == "FOLDERS":
+                match format:
+                    case "NAME":
+                        data = self.controller.get_folders_by_name(input)
+
+                    case "PATTERN":
+                        data = self.controller.get_folders_by_pattern(input)
+
+            return (data, False)
+
+        except Exception as e:
+            self.controller.show_dialog(
+                f"UNKNOWN ERROR OCCURED | {str(e)}",
+                "c",
+                is_dialog=False
+            )
+
+            return (data, True)                 # FAILED = True
+
 
     def set_icon(self, widget: QWidget, name: str, size=(18, 18)) -> str:
 
